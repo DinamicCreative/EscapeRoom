e29516a3e16b14314cf6e39f2624c1b9f890fb24
isadoradomingos <107214523+isadoradomingos@users.noreply.github.com>
2023-03-27 08:51:33 -0300

    #17

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+
+// Configuração do jogo
 import config from "./config.js";
+
+// Cena de abertura
+import CenaDeAbertura from ".cena.js";
+
+// Cena principal
+import principal from "./principal.js";
+
+// Cena de encerramento
+import encerramento from "./encerramento.js";
 
 class Game extends Phaser.Game {
   constructor() {
     super(config);
+    //
+    // Carregar as cenas
+    this.scene.add ("abertura", abertura);
+    this.scene.add("principal", principal);
+    this.scene.add("encerramento", encerramento);
+    //
+    // Iniciar pela cena de abertura
   }
 }
 
