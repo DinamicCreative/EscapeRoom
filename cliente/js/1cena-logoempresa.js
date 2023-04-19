72a4d811325ecb9d424f80ceab184ed4d32b7b55
Isadora Domingos <isasundays@gmail.com>
2023-04-19 12:01:35 +0000

    timer falho

--- cliente/js/1cena-logoempresa.js
+++ cliente/js/1cena-logoempresa.js
@@ -8,14 +8,38 @@ export default class logo_empresa extends Phaser.Scene {
   }
 
   create() {
-    this.faca = this.add
+    this.vela = this.add
       .sprite(225, 400, "logoempresa")
       .setInteractive()
       .on("pointerdown", () => {
-        this.faca.destroy();
+        this.vela.destroy();
         this.game.scene.start("capa-do-jogo");
       });
+
+    /* Contagem regressiva */
+    this.timer = 10;
+
+    this.timedEvent = this.time.addEvent({
+      delay: 1000,
+      callback: this.countdown,
+      callbackScope: this,
+      loop: true,
+    });
   }
 
   upload() {}
+
+  countdown() {
+    /* Reduz o contador em 1 segundo */
+    this.timer -= 1;
+
+    /* Quando chegar a zero, troca a cena */
+    if (this.timer === 0) {
+      this.timedEvent.destroy();
+      this.timerText.destroy();
+      this.game.scene.start("capa-do-jogo.js");
+    } else {
+      this.timerText.setText(this.timer);
+    }
+  }
 }
