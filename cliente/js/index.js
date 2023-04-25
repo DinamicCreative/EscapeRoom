8d04140267df27955317181bcfa1118f3cb513ad
Isadora Domingos <isasundays@gmail.com>
2023-04-25 14:18:20 +0000

    proxima cena 1

--- cliente/js/index.js
+++ cliente/js/index.js
@@ -4,7 +4,7 @@ import logo_empresa from "./1cena-logoempresa.js";
 import capa_do_jogo from "./2capa-do-jogo.js";
 import cena_carregamento from "./3cena-carregamento.js";
 import aviso_hora from "./4cena-aviso-hora.js";
-//import principal from "./5cena-nivel1.js";
+import principal from "./5cena-nivel1.js";
 //import fim_do_jogo from "./cena-finaltriste.js";
 //import final_feliz from "./10cena-finalfeliz.js";
 //import logoempresa from "./1cena-logoempresa.js";
@@ -17,7 +17,7 @@ class Game extends Phaser.Game {
     this.scene.add("capa-do-jogo", capa_do_jogo);
     this.scene.add("cena-carregamento", cena_carregamento);
     this.scene.add("aviso-hora", aviso_hora);
-    //this.scene.add("principal", principal);
+    this.scene.add("cenanivel1", cenanivel1);
     //this.scene.add("fim-do-jogo", fim_do_jogo);
     //this.scene.add("final-feliz", final_feliz);
 
