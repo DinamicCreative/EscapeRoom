d9b70ece911bf9770af41f05b854937fffbe0724
Isadora Domingos <isasundays@gmail.com>
2023-05-12 11:19:20 +0000

    dowload das imagens na segunda cena

--- cliente/js/2capa-do-jogo.js
+++ cliente/js/2capa-do-jogo.js
@@ -18,8 +18,99 @@ export default class capa_do_jogo extends Phaser.Scene {
       "botaostand",
       "./assets/cenaqualjogador/botaostand.png"
     );
-    
-//preload de imagens jogador 1 e jogador 2
+
+    this.load.spritesheet("vela1", "./assets/vela.png", {
+      frameWidth: 450,
+      frameHeight: 800,
+    });
+
+    this.load.image(
+      "botao-desistencia",
+      "./assets/desistir/botaodesistencia.png"
+    );
+    this.load.image(
+      "caixa-desistencia",
+      "./assets/desistir/caixadesistencia.png"
+    );
+
+    this.load.image(
+      "botao-nao",
+      "./assets/desistir/botaonao.png"
+    );
+
+    this.load.image(
+      "botao-sim",
+      "./assets/desistir/botaosim.png"
+    );
+
+    this.load.image(
+      "aviso-hora1",
+      "./assets/cenaavisohora.png"
+    );
+
+    this.load.image(
+      "j1n1",
+      "./assets/jogador1/j1-nivel1.png"
+    );
+
+    this.load.image(
+      "j1n2",
+      "./assets/jogador1/j1-nivel2.png"
+    );
+
+    this.load.image(
+      "j1n3", 
+      "./assets/jogador1/j1-nivel3.png"
+    );
+
+    this.load.image(
+      "j1n4",
+      "./assets/jogador1/j1-nivel4.png"
+    );
+
+    this.load.image(
+      "j1n5",
+      "./assets/jogador1/j1-nivel5.png"
+    );
+
+    this.load.image(
+      "j2n1",
+      "./assets/jogador2/j2-nivel1.png"
+    );
+
+    this.load.image(
+      "j2n2",
+      "./assets/jogador2/j2-nivel2.png"
+    );
+
+    this.load.image(
+      "j2n3",
+      "./assets/jogador2/j2-nivel3.png"
+    );
+
+    this.load.image(
+      "j2n4",
+      "./assets/jogador2/j2-nivel4.png"
+    );
+
+    this.load.image(
+      "j2n5",
+      "./assets/jogador2/j2-nivel5.png"
+    );
+
+    this.load.image(
+      "finaldesistiu",
+      "./assets/finais/desistiu.png"
+    );
+
+    this.load.image("finalfeliz",
+      "./assets/finais/ganhou.png"
+    );
+
+    this.load.image(
+      "finaltriste",
+      "./assets/finais/perdeu.png"
+    );
 
   }       
 
--- cliente/js/finais/finaltriste.js
+++ cliente/js/finais/finaltriste.js
@@ -4,23 +4,11 @@ export default class finaltriste extends Phaser.Scene {
   }
 
   preload() {
-    this.load.spritesheet("faca", "./assets/faca.png");
+    this.load.image("finaltriste", "./assets/finais/perdeu.png");
   }
 
   create() {
-    this.imagem = this.add
-      .sprite(400, 225, "faca")
-      .setTint(0xff0000)
-      .setInteractive()
-      .on("pointerdown", () => {
-        this.imagem.destroy();
-        this.texto.destroy();
-        this.game.scene.start("principal");
-      });
 
-    this.texto = this.add.text(490, 50, "Fim do jogo.", {
-      fill: "#000000",
-    });
   }
 
   upload() {}
