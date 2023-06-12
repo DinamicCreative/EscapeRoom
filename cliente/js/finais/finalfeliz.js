c8673a94183b0e360f2df328a12daee4ed7fa7e4
Isadora Domingos <isasundays@gmail.com>
2023-06-12 10:38:02 +0000

    musica fim bom

--- cliente/js/finais/finalfeliz.js
+++ cliente/js/finais/finalfeliz.js
@@ -5,9 +5,15 @@ export default class finalfeliz extends Phaser.Scene {
 
   preload() {
     this.load.image("finalfeliz", "./assets/finais/ganhou.png");
+    this.load.audio("fim", "./assets/fim.mp3");
+
   }
 
   create() {
+
+    this.fim = this.sound.add("fim");
+    this.fim.play();
+
     this.finalfeliz = this.add.image(225, 400, "finalfeliz");
     let conn = this.game.localConnection || this.game.remoteConnection;
     conn.close();
